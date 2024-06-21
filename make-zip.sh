213f6de761f535355f51db96c2baf5528b2ae518
SVR Kalluru <raghuk.vit@gmail.com>
2024-06-21 20:32:38 +0530

    m

--- make-zip.sh
+++ make-zip.sh
@@ -1,7 +1,7 @@
 for i in catalogue; do
   git clone https://github.com/roboshop-devops-project-v3/$i.git
   rm -rf $i/.git
-  zip -r $i.zip $i
+  zip -r $i.zip $i/*
   aws s3 rm s3://roboshop-artifacts/$i-v3.zip
   aws s3 cp $i.zip s3://roboshop-artifacts/$i-v3.zip
   rm -rf $i*
