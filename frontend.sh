135610926854dcb7ec74cbdf0f3f039d8420221b
SVR Kalluru <raghuk.vit@gmail.com>
2024-06-23 08:23:47 +0530

    m

--- frontend.sh
+++ frontend.sh
@@ -4,7 +4,7 @@ dnf module enable nginx:1.24 -y
 dnf install nginx -y
 rm -rf /usr/share/nginx/html/*
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+curl -o /tmp/frontend-v3.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend-v3.zip
 
